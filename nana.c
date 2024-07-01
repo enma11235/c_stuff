c151c7fb38c8d2a65d5d4617c0633793b7f239df
Enmanuel Rodriguez <enmaundostres@gmail.com>
2024-07-01 19:12:25 -0300

    como el sol

--- nana.c
+++ nana.c
@@ -6,7 +6,11 @@ int main() {
     printf("Escribe cualquier cosa y luego presiona 'enter'.\n");
     printf("Lo que escribas sera guardado en una carpeta especial y leido por mi >:)\n\n");
 
+    //creamos un nuevo archivo en la carpeta de mensajitos
+    //podemos crear un archivito que nos diga el número del mensaje más alto
     FILE* mensajito = fopen("/home/enma/mensajitos/mensaito1", "w");
+    char mensaje[50];
+    scanf("%s", mensaje);
     fclose(mensajito);
     return 0;
 }
--- readString.c
+++ readString.c
@@ -1,2 +1,35 @@
 void readString() {
+    //creamos una estructura de lista
+    struct nodo {
+	char c;
+	struct nodo* sig;
+    }
+
+    struct lista {
+	struct nodo* inicio;
+	struct nodo* final;
+    }
+
+    //instanciamos un nuevo cabezal
+    struct lista;
+    cabezal.inicio = new steuct lista;
+    cabezal.final = new;
+    
+    //leemos los caracteres
+    char c;
+    while ((c = getchar()) != '\n'){
+	lista.final.sig = new struct nodo;
+	lista.final = lista.final.sig;
+	lista.final.c = c;
+	list.final.sig = NULL;
+    }
+
+    //volcamos los caracteres leidos en u string
+    struct nodo pointer = lista.inicio;
+    char string[lista.size];
+    for(struct nodo* i; i != NULL; i = i.sig){
+	string[i.pos] = i.c;
+    }
+
+    //liberamos la lista
 }
