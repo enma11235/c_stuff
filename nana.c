cec8a05030482fcde1f298638ad8275660ec21dd
Enmanuel Rodriguez <enmaundostres@gmail.com>
2024-07-02 14:04:56 -0300

    blanquita

--- nana.c
+++ nana.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
+#include "./readString.c"
 
 int main() {
-    printf("Hola Morwenna.\n");
-    printf("Bienvenida a mi computadora.\n");
-    printf("Escribe cualquier cosa y luego presiona 'enter'.\n");
-    printf("Lo que escribas sera guardado en una carpeta especial y leido por mi >:)\n\n");
-	const char* name = readString();
-
-    //creamos un nuevo archivo en la carpeta de mensajitos
-    //podemos crear un archivito que nos diga el número del mensaje más alto
-    FILE* mensajito = fopen("/home/enma/mensajitos/mensaito1", "w");
-    char mensaje[50];
-    scanf("%s", mensaje);
-    fclose(mensajito);
-    return 0;
+  printf("Hola Personita.\n");
+  printf("Bienvenida a mi computadora :)\n");
+  printf("Por favor escribe un mensajito y presiona [ENTER].\n");
+  
+  // abrimos un archivo en la carpeta "mensajitos"
+  FILE* fd = fopen("/home/enma/mensajitos/mensaito1", "w");
+  //creamos un array de 50 caracteres para almacenar el mensaje
+  char* mensaje = getString();
+  //leemos el mensaje
+  scanf("%s", mensaje);
+  //escribimos en el archivo
+  fwrite(mensaje, sizeof(char), 50, fd);
+  //cerramos el archivo
+  fclose(fd);
+  return 0;
 }
--- readString.c
+++ readString.c
@@ -1,47 +1,59 @@
-const char* readString() {
-    //creamos una estructura de lista
+#include <stdio.h>
+#include <stdlib.h>
+
+char* getString() {
+
+    //variables
+    char c;
+    int pos;
+
+    //estructura de lista 
     struct nodo {
 	char c;
 	struct nodo* sig;
-    }
+    };
 
     struct lista {
 	struct nodo* inicio;
 	struct nodo* final;
 	int size;
-    }
+    };
 
     //instanciamos una nueva lista
-    struct lista buffer;
-    buffer.inicio = (struct nodo*)malloc(sizeof(struct nodo));
-    buffer.final = lista.inicio;
-    buffer.size = 0;
-    
+    struct lista lista;
+    lista.inicio = (struct nodo*)malloc(sizeof(struct nodo));
+    lista.final = lista.inicio;
+    lista.size = 0;
+
     //leemos el primer caracter
-    char c;
-    c = getchar();
-    buffer.inicio->c = c;
-    buffer.size = 1;
+    scanf("%c", &c);
 
     //loopeamos hasta encontrar un salto de linea
-    while ((c = getchar()) != '\n'){
-	buffer.final.sig = (struct nodo*)malloc(sizeof(struct nodo));
-	buffer.final = lista.final->sig;
-	buffer.final->c = c;
-	buffer.final->sig = NULL;
-	buffer.size++;
+    while (c != '\n'){
+	lista.final->sig = (struct nodo*)malloc(sizeof(struct nodo));
+	lista.final = lista.final->sig;
+	lista.final->c = c;
+	lista.final->sig = NULL;
+	lista.size++;
+	scanf("%c", &c);
     }
 
-    //volcamos los caracteres leidos en u string
-    int pos = 0;
-    char string[buffer.size + 1];
-    for(struct nodo* i = buffer.inicio; i != NULL; i = i->sig){
+    //copiamos los caracteres de la lista a un array
+    pos = 0;
+    char* string = (char*)malloc(sizeof(char)*lista.size + 1);
+    for(struct nodo* i = lista.inicio; i != NULL; i = i->sig){
 	string[pos] = i->c;
 	pos++;
     }
     string[pos] = '\0';
 
     //liberamos la lista
+    struct nodo* i = lista.inicio;
+    while(i != NULL) {
+	struct nodo* aux = i;
+	i = i->sig;
+	free(aux);
+    }
 
     //retornamos el string
     return string;
